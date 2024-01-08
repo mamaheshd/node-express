999943c922cbba9116885f5398ff8ea03fae0871
mamaheshd <mamaheshd@gmail.com>
2024-01-08 20:29:58 +0545

    All files manageed

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express=require('express')
 const app=express()
 require('dotenv').config()
 
+const catrgoryRoute=require('./routes/categoryRoute')
+
 // app.use('/',(req,res)=>{
 //     // res.json({message:'This is a express server'})
 // })
 
+app.use('/api',catrgoryRoute)
 
 const port=process.env.PORT || 5000
 app.listen(port,()=>{
